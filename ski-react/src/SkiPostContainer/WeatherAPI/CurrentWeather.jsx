7df509779510671355c1177de07a0bb7af3c81af
Daniel Becker <danielbecker@MacBook-Pro-5.local>
2018-11-08 10:54:04 -0700

    updating master 2

--- ski-react/src/SkiPostContainer/WeatherAPI/CurrentWeather.jsx
+++ ski-react/src/SkiPostContainer/WeatherAPI/CurrentWeather.jsx
@@ -3,14 +3,11 @@ import TempConverter from "./TempConverter/TempConverter"
 
 
 const CurrentWeather = (props) => {
-    console.log(props.theWeather.weather)
     return(
       <main>
           <h2>Weather Report:</h2>
             <h5>Temp:</h5>
-                <p>
                     <TempConverter temp={props.theWeather.main.temp} />
-                </p>
             <h5>Summary:</h5>    
                 <p>
                     {JSON.stringify(props.theWeather.weather[0].description)}
